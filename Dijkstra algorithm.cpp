ff9e9676d1991e455f98734e5950575136566bca
tomaszrogala <41085541+tomaszrogala@users.noreply.github.com>
2018-11-02 16:59:31 +0100

    Update Dijkstra algorithm.cpp

--- Dijkstra algorithm.cpp
+++ Dijkstra algorithm.cpp
@@ -7,8 +7,7 @@ struct Vertex
 	int data;
 	bool open;
 
-	Vertex()
-	{
+	Vertex(){
 		open = true;
 	}
 };
@@ -20,28 +19,22 @@ struct Graph
 		
 	void search_path(int vert_numb, int begin_index, int end_index);
 
-	Graph(int vert_numb) 
-	{
+	Graph(int vert_numb) {
 		Dijkstr = new Vertex[vert_numb];
 
 		//TWO-DIMENSIONAL TRIANGULAR ARRAY:
 		M = new int*[vert_numb]; 
 
-		for (int i = 0; i < vert_numb; i++) 
-		{
+		for (int i = 0; i < vert_numb; i++) {
 			M[i] = new int[i+1];
 		}
 
-		for (int i = 0; i < vert_numb; i++)
-		{
-			for (int j = 0; j <= i; j++)
-			{
-				if (i == j)
-				{
+		for (int i = 0; i < vert_numb; i++) {
+			for (int j = 0; j <= i; j++) {
+				if (i == j) {
 					M[i][j] = 0;
 				}
-				else
-				{
+				else {
 					M[i][j] = INT_MAX;
 				}
 			}
@@ -54,47 +47,34 @@ void Graph::search_path(int vert_numb, int begin_index, int end_index)
 	bool flag = false;
 	int min = INT_MAX;
 
-	for (int i = 0; i < vert_numb; i++)
-	{
+	for (int i = 0; i < vert_numb; i++) {
 		Dijkstr[i].data = INT_MAX;
 		Dijkstr[i].open = true;
 	}
 
-	while (end_index != begin_index)
-	{
-		for (int i = 0; i < vert_numb; i++)
-		{
+	while (end_index != begin_index) {
+		for (int i = 0; i < vert_numb; i++) {
 			if (Dijkstr[i].open == true )
 			{
-				if (begin_index > i)
-				{
-					if (M[begin_index][i] != INT_MAX)
-					{
-						if (Dijkstr[begin_index].data == INT_MAX)
-						{
+				if (begin_index > i) {
+					if (M[begin_index][i] != INT_MAX) {
+						if (Dijkstr[begin_index].data == INT_MAX) {
 							Dijkstr[i].data = M[begin_index][i];
 						}
-						else
-						{
-							if (Dijkstr[i].data > M[begin_index][i] + Dijkstr[begin_index].data)
-							{
+						else {
+							if (Dijkstr[i].data > M[begin_index][i] + Dijkstr[begin_index].data) {
 								Dijkstr[i].data = M[begin_index][i] + Dijkstr[begin_index].data;
 							}
 						}
 					}
 				}
-				else
-				{
-					if (M[i][begin_index] != INT_MAX)
-					{
-						if (Dijkstr[begin_index].data == INT_MAX)
-						{
+				else {
+					if (M[i][begin_index] != INT_MAX) {
+						if (Dijkstr[begin_index].data == INT_MAX) {
 							Dijkstr[i].data = M[i][begin_index];
 						}
-						else
-						{
-							if (Dijkstr[i].data > M[i][begin_index] + Dijkstr[begin_index].data)
-							{
+						else {
+							if (Dijkstr[i].data > M[i][begin_index] + Dijkstr[begin_index].data) {
 								Dijkstr[i].data = M[i][begin_index] + Dijkstr[begin_index].data;
 							}
 						}
@@ -105,16 +85,13 @@ void Graph::search_path(int vert_numb, int begin_index, int end_index)
 		
 		Dijkstr[begin_index].open = false;
 		//SEARCHING FOR THE SHORTEST PATH
-		for (int i = 0; i < vert_numb; i++)
-		{
-			if (Dijkstr[i].data < min && Dijkstr[i].open == true)
-			{
+		for (int i = 0; i < vert_numb; i++) {
+			if (Dijkstr[i].data < min && Dijkstr[i].open == true) {
 				min = Dijkstr[i].data;
 				begin_index = i;
 			}
 		}
-		if (min == INT_MAX)
-		{
+		if (min == INT_MAX) {
 			std::cout << "no path" << '\n';
 			flag = true;
 			break;
@@ -127,30 +104,25 @@ void Graph::search_path(int vert_numb, int begin_index, int end_index)
 
 
 //INPUT FUNCTIONS:
-int exp(int base, int index)
-{
+int exp(int base, int index) {
 	int x = 1;
 
-	for (int i = 0; i < index; i++)
-	{
+	for (int i = 0; i < index; i++) {
 		x *= base;
 	}
 
 	return x;
 }
 
-int input_int(char *&pointer)
-{
+int input_int(char *&pointer) {
 	int x = 0;
 	int k = 0;
 
-	while (*pointer != ' ' && *pointer != '\n')
-	{
+	while (*pointer != ' ' && *pointer != '\n') {
 		k++;
 		pointer++;
 	}
-	for (int i = 0; i < k; i++)
-	{
+	for (int i = 0; i < k; i++) {
 		pointer--;
 		x = x + ((*pointer - 48) * exp(10, i));
 	}
@@ -160,31 +132,23 @@ int input_int(char *&pointer)
 }
 
 //COUNTING FUNCTION:
-int count_array(int array[], int n, int count) 
-{
-	if (array[n] != -858993460) 
-	{
+int count_array(int array[], int n, int count) {
+	if (array[n] != -858993460) {
 		return count_array(array, n + 1, count + 1);
 	}
 
 	return count;
 }
 
-bool correct(int tab_out[], int vert_numb, bool flag)
-{
-	for (int i = 0; i < count_array(tab_out, 0, 0); i++)
-	{
-		if (flag == false)
-		{
-			if ((tab_out[i] >= vert_numb || tab_out[i] < 0) && (i + 1) % 3 != 0)
-			{
+bool correct(int tab_out[], int vert_numb, bool flag) {
+	for (int i = 0; i < count_array(tab_out, 0, 0); i++) {
+		if (flag == false) {
+			if ((tab_out[i] >= vert_numb || tab_out[i] < 0) && (i + 1) % 3 != 0) {
 				return false;
 			}
 		}
-		else
-		{
-			if (tab_out[i] >= vert_numb || tab_out[i] < 0)
-			{
+		else {
+			if (tab_out[i] >= vert_numb || tab_out[i] < 0) {
 				return false;
 			}
 		}	
@@ -196,23 +160,19 @@ bool correct(int tab_out[], int vert_numb, bool flag)
 void in(char tab_in[],int line_numb, char *&pointer, int tab_out[], FILE *file, int vert_numb, bool flag)
 {
 	int k;
-	do
-	{
+	do {
 		k = 0;
-		for (int i = 0; i < line_numb; i++)
-		{
+		for (int i = 0; i < line_numb; i++) {
 			fgets(tab_in, 100, file);
 			pointer = &tab_in[0];
 
-			for (int j = 0; *pointer != '\0'; j++)
-			{
+			for (int j = 0; *pointer != '\0'; j++) {
 				tab_out[k] = input_int(pointer);
 				pointer++;
 				k++;
 			}
 		}
-		if (correct(tab_out, vert_numb, flag) == false)
-		{
+		if (correct(tab_out, vert_numb, flag) == false) {
 			std::cout << "WORKS ONLY FOR FIGURES <0, VERT_NUMB)\nPlease enter the connections again:\n";
 		}
 	} while (correct(tab_out, vert_numb, flag) == false);
@@ -238,14 +198,11 @@ int main()
 	in(vert, line_numb, pointer, vert_out, stdin, vert_numb, flag);
 
 	//GRAPH CREATING:
-	for (int i = 0; i < line_numb*3; i = i+3)
-	{
-		if (vert_out[i] > vert_out[i + 1])
-		{
+	for (int i = 0; i < line_numb*3; i = i+3) {
+		if (vert_out[i] > vert_out[i + 1]) {
 			g.M[vert_out[i]][vert_out[i+1]] = vert_out[i + 2];
 		}
-		else
-		{		
+		else {		
 			g.M[vert_out[i+1]][vert_out[i]] = vert_out[i + 2];
 		}
 	}
@@ -261,8 +218,7 @@ int main()
 
 	std::cout << "SOLUTION:\n";
 	//Dijkstra:
-	for (int i = 0; i < line_numb*2; i=i+2)
-	{
+	for (int i = 0; i < line_numb*2; i=i+2) {
 		g.search_path(vert_numb, rout_out[i+0], rout_out[i+1]);
 	}
 
